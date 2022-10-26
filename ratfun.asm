9e87c43e285053e5821004d718a02f469a834fc2
gentcxc <crcartagit@gmail.com>
2022-10-26 19:38:20 -0400

    was trying to use a memory value to do arithmetic, added lw command, finish fixing tomorrow

--- ratfun.asm
+++ ratfun.asm
@@ -8,17 +8,12 @@
 #########################################################
 
 
-    .data
-x:      .word 3         #address 0x10000000 , "x" will serve as x value
-error:  .word 0         #address 0x10000004 , "error" set to 0
-ratio:  .word 0         #address 0x10000008 , "ratio" set to 0
-remain: .word 0         #address 0x1000000c , "remain" set to 0 
-
     .text
     .globl main
 
 main:
     lui, $10, 0x1000    #register 10 contains address 0x1000 (x)
+    lw   $11, 0($10)
     or   $29, $0, 0x1   #load a 1 into register 29
     or   $8, $0, 0x8    #load an 8 into register 8, begin generating first parenthese
     mult $10, $8        #x(8)
@@ -45,8 +40,7 @@ main:
     mfhi $2
     sll $0, $0, 0       #nop
     sll $0, $0, 0       #nop
-    sw $1, 8($10)
-    sw $2, 12($10)
+    sw $8,0($10)
     j  cont            #jump to end of program
     sll $0, $0, 0       #nop
 equal:
@@ -55,5 +49,10 @@ equal:
     sll $0, $0, 0       #nop
 cont: sll $0, $0, 0
 
+    .data
+x:      .word 3         #address 0x10000000 , "x" will serve as x value
+error:  .word 0         #address 0x10000004 , "error" set to 0
+ratio:  .word 0         #address 0x10000008 , "ratio" set to 0
+remain: .word 0         #address 0x1000000c , "remain" set to 0 
 
 ##EOF
