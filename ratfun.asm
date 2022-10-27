0a738f57b1044f99e8c3a681674aa623b01a2d98
gentcxc <crcartagit@gmail.com>
2022-10-27 12:56:52 -0400

    finishing bufixing, mostly done, has stack problem

--- ratfun.asm
+++ ratfun.asm
@@ -16,19 +16,19 @@ main:
     lw   $11, 0($10)
     or   $29, $0, 0x1   #load a 1 into register 29
     or   $8, $0, 0x8    #load an 8 into register 8, begin generating first parenthese
-    mult $10, $8        #x(8)
+    mult $11, $8        #x(8)
     mflo $5             #put product in register 5
     sll $0, $0, 0       #nop
     sll $0, $0, 0       #nop
     addiu $5, $5, -3    #8x-3
-    mult $5, $10        #(8x-3)(x)
+    mult $5, $11        #(8x-3)(x)
     mflo $5             #put product in register 5
     sll $0, $0, 0       #nop
     sll $0, $0, 0       #nop
     addiu $5, 0xC       #(8x^2 - 3x)+12 , first parenthese done
-    or $6, $0, $10      #load x into register 10 , begin generating second parenthese
+    or $6, $0, $11      #load x into register 6 , begin generating second parenthese
     addiu $6, 0x2       #register 6 += 2
-    mult $6, $10        #(x+2)(x)
+    mult $6, $11        #(x+2)(x)
     mflo $6             #put product into register 6
     sll $0, $0, 0       #nop
     sll $0, $0, 0       #nop
@@ -40,7 +40,8 @@ main:
     mfhi $2
     sll $0, $0, 0       #nop
     sll $0, $0, 0       #nop
-    sw $8,0($10)
+    sw $1,8($10)
+    sw $2,12($10)
     j  cont            #jump to end of program
     sll $0, $0, 0       #nop
 equal:
@@ -50,7 +51,7 @@ equal:
 cont: sll $0, $0, 0
 
     .data
-x:      .word 3         #address 0x10000000 , "x" will serve as x value
+x:      .word 6         #address 0x10000000 , "x" will serve as x value
 error:  .word 0         #address 0x10000004 , "error" set to 0
 ratio:  .word 0         #address 0x10000008 , "ratio" set to 0
 remain: .word 0         #address 0x1000000c , "remain" set to 0 
