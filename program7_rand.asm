988133b9d03f790b5213e2900083dddcabf8b7b9
TerrariaIssues <crcarta@gmail.com>
2022-11-16 17:53:05 -0500

    adding to program 7

--- program7_rand.asm
+++ program7_rand.asm
@@ -9,11 +9,25 @@
 
 
 .data
-prompt: .asciiz "\nInput Seed: "
-prompt1: .asciiz "How Many? : "
+string: .asciiz "\nInput Seed: "
+string1: .asciiz "How Many? : "
 
 
     .text 
     .globl main
 main:
+    li $v0, 4
+    la $a0, string
+    syscall
+    li $v0, 5
+    syscall
+    move $s0, $v0
+    li $v0, 4
+    la $a0, string1
+    syscall
+    li $v0, 5
+    syscall
+    move $s1, $v0##input over
 
+
+https://chortle.ccsu.edu/AssemblyTutorial/Chapter-21/ass21_2.html
